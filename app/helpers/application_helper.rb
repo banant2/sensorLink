43e2bcf789e8cf2c365b19787a9b950e219fda62
banant2 <banant2@gmail.com>
2017-08-21 23:42:40 -0700

    added menu builder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 module ApplicationHelper
+  def buildMenu(array @menu_array, @is_sub=FALSE)
+  {
+     $ul_attrs = @is_sub ? 'class="dropdown-menu"' : 'class="nav navbar-nav"'
+     $menu = "<ul $ul_attrs>";
 
+     foreach(@menu_array as $id => $attrs)
+     {
+       $sub = isset($attrs['sub'])
+         ? buildMenu($attrs['sub'], TRUE)
+         : null;
+         $li_attrs = $sub ? 'class="dropdown"' : null;
+      $a_attrs  = $sub ? 'class="dropdown-toggle" data-toggle="dropdown"' : null;
+      $carat    = $sub ? '<span class="caret"></span>' : null;
+      $menu .= "<li $li_attrs>";
+      $menu .= "<a href='$id' $a_attrs>${attrs['text']}$carat</a>$sub";
+      $menu .= "</li>";
+     }
+
+   return $menu . "</ul>";
+  }
 end
