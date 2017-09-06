433c2e4fd02a17693a899fa2d03478a4404badb0
banant2 <banant2@gmail.com>
2017-09-06 11:34:53 -0700

    added dashboard features

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,24 @@
 class WelcomeController < ApplicationController
   def show
   end
+
   def dashboard
-     @authenticated_plants = Plant.where(owner_id: current_user.id)
-     puts @authenticated_plants.inspect
+    authenticated_plants = Plant.select('sensorID', "name").where(owner_id: current_user.id)
+
+    l = authenticated_plants.length
+    @plant_auth = Array.new(l){Array.new(2)}
+
+    i = 0
+    while i < l do
+      @plant_auth[i][0] = authenticated_plants[i].sensorID
+      @plant_auth[i][1] = authenticated_plants[i].name
+      i+=1
+    end
+
+
   end
-
   private
-   def plant_qualifier
+    def plant_qualifier
 
-   end
+    end
 end
