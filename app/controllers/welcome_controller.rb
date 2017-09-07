f57b44de21b15e4ed6ce003ff5661edaa48b3afb
banant2 <banant2@gmail.com>
2017-09-06 23:59:48 -0700

    added dashboard features

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,14 +6,20 @@ class WelcomeController < ApplicationController
     authenticated_plants = Plant.select('sensorID', "name").where(owner_id: current_user.id)
 
     l = authenticated_plants.length
-    @plant_auth = Array.new(l){Array.new(2)}
+    @plant_auth = Array.new(l){Array.new(3)}
 
     i = 0
     while i < l do
       @plant_auth[i][0] = authenticated_plants[i].sensorID
       @plant_auth[i][1] = authenticated_plants[i].name
+      placeholder = EnvDatum.select('soil_conduct').where(sensorID: authenticated_plants[i].sensorID).order(:record_time).reverse_order.limit(1)
+      puts placeholder.inspect
+      #@plant_auth[i][2] =
+      #puts placeholder[0].soil_conduct
+      #puts @plant_auth[i][2].inspect
       i+=1
     end
+
 
 
   end
